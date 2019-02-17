f1d28076d6c43fb774397f2db7ab3ce661cae5b8
Luke Bennett <luke.bennett88@gmail.com>
2019-02-17 17:19:33 +1100

    Split up CSS into multiple files and update layout component

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,7 +4,9 @@ import { StaticQuery, graphql } from 'gatsby';
 
 import Header from './header';
 import Footer from './footer';
-import '../css/style.css';
+import '../css/tailwind.css';
+import '../css/typography.css';
+import '../css/global.css';
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -20,10 +22,10 @@ const Layout = ({ children }) => (
     render={data => (
       <div className="flex flex-col font-sans min-h-screen text-grey-darkest">
         <Header siteTitle={data.site.siteMetadata.title} />
-        <main className="flex flex-col flex-1 md:justify-start max-w-md mx-auto w-full">
+        <main className="flex flex-col flex-1 md:justify-start w-full">
           {children}
         </main>
-        <Footer />
+        <Footer siteTitle={data.site.siteMetadata.title} />
       </div>
     )}
   />
