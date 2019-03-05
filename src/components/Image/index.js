492c5c5fc35d4c42245c59fa7948d86a0cd9b8fe
Luke Bennett <luke.bennett88@gmail.com>
2019-03-05 15:56:42 +1100

    Add images

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -17,7 +17,7 @@ const Image = () => (
   <StaticQuery
     query={graphql`
       query {
-        placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+        placeholderImage: file(relativePath: { eq: "img01.jpg" }) {
           childImageSharp {
             fluid {
               ...GatsbyImageSharpFluid_withWebp_tracedSVG
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,21 +5,31 @@ import Layout from '../components/Layout';
 import Image from '../components/Image';
 import SEO from '../components/SEO';
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <div className="max-w-2xl mx-auto px-4 py-8 w-full">
       <div className="max-w-md mx-auto w-full">
-        <h1>Mid Coast Pluge Pools</h1>
-        <p>Welcome to your new Gatsby site.</p>
-        <p>Now go build something great.</p>
-        <div className="max-w-sm mx-auto w-full">
+        <h1>{data.site.siteMetadata.title}</h1>
+        <p>
+          <Link to="/style-sample/">Check out the Style Sample page</Link>
+        </p>
+        <div className="w-full">
           <Image />
         </div>
-        <Link to="/style-sample/">Check out the Style Sample page</Link>
       </div>
     </div>
   </Layout>
 );
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`;
 
 export default IndexPage;
