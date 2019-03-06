48f1219bbe4087c492d9355231d6d4c30a80275f
Luke Bennett <luke.bennett88@gmail.com>
2019-03-06 20:16:49 +1100

    Comment out unused markup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Link } from 'gatsby';
+// import { Link } from 'gatsby';
 
 import Layout from '../components/Layout';
-import Image from '../components/Image';
+// import Image from '../components/Image';
 import SEO from '../components/SEO';
 
 const IndexPage = ({ data }) => (
@@ -10,13 +10,13 @@ const IndexPage = ({ data }) => (
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <div className="max-w-2xl mx-auto px-4 py-8 w-full">
       <div className="max-w-md mx-auto w-full">
-        <h1>{data.site.siteMetadata.title}</h1>
+        {/* <h1>{data.site.siteMetadata.title}</h1>
         <p>
           <Link to="/style-sample/">Check out the Style Sample page</Link>
         </p>
         <div className="w-full">
           <Image />
-        </div>
+        </div> */}
       </div>
     </div>
   </Layout>
