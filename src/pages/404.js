f4422bdd78d1bf6c3cfc1e7542458388aec53801
Luke Bennett <luke.bennett88@gmail.com>
2019-03-08 13:34:02 +1100

    Update 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,11 +4,10 @@ import Layout from '../components/Layout';
 import SEO from '../components/SEO';
 
 const NotFoundPage = () => (
-  <Layout>
+  <div className="flex flex-col h-screen items-center justify-center max-w-2xl mx-auto p-8 text-blue-grey-800 w-full">
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+    <h1>404: PAGE NOT FOUND</h1>
+  </div>
 );
 
 export default NotFoundPage;
