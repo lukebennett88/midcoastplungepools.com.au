3adee9d2f81dde5eaad9986319046b243318776b
Luke Bennett <luke.bennett88@gmail.com>
2019-03-10 14:29:56 +1100

    Change theme color and add placeholder favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,11 +15,11 @@ module.exports = {
         short_name: `GatsbyJS`,
         start_url: `/`,
         background_color: `#fff`,
-        theme_color: `#189473`,
+        theme_color: `#090C96`,
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: `standalone`,
-        icon: `src/images/logo.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-postcss`,
