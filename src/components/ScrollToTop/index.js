1e63af649b05e507b374297cad0316a611d83928
Luke Bennett <luke.bennett88@gmail.com>
2019-03-12 18:32:45 +1100

    Fix icon in scroll to top button

--- src/components/ScrollToTop/index.js
+++ src/components/ScrollToTop/index.js
@@ -11,7 +11,6 @@ const ScrollToTop = () => (
       className="fill-current text-blue-vivid-600 h-full w-full"
     >
       <path
-        className="text-white underline"
         fillRule="evenodd"
         d="M8.7 13.7a1 1 0 1 1-1.4-1.4l4-4a1 1 0 0 1 1.4 0l4 4a1 1 0 0 1-1.4 1.4L12 10.42l-3.3 3.3z"
       />
