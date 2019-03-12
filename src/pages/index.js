54965a3a984ae73a19176fe968195a57133dc870
Luke Bennett <luke.bennett88@gmail.com>
2019-03-12 20:06:19 +1100

    Style home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,9 @@ import { graphql, Link } from 'gatsby';
 
 import Layout from '../components/Layout';
 import SEO from '../components/SEO';
-import Image from '../components/Image';
+import HeroImage from '../components/HeroImage';
+import ContactForm from '../components/Form';
+import ContactImage from '../components/ContactImage';
 import Logo from '../images/logo-white.svg';
 
 const IndexPage = ({ data }) => (
@@ -26,6 +28,11 @@ const IndexPage = ({ data }) => (
         title={data.site.siteMetadata.title}
         author={data.site.siteMetadata.author}
       />
+      <HeroContact
+        phone={data.site.siteMetadata.phone}
+        phone_formatted={data.site.siteMetadata.phone_formatted}
+        email={data.site.siteMetadata.email}
+      />
       <Contact
         phone={data.site.siteMetadata.phone}
         phone_formatted={data.site.siteMetadata.phone_formatted}
@@ -38,7 +45,7 @@ const IndexPage = ({ data }) => (
 const Hero = ({ title }) => (
   <div className="hero flex items-center max-w-3xl mx-auto overflow-hidden relative w-full">
     <div className="hero-container">
-      <Image className="hero-img" />
+      <HeroImage className="hero-img" />
     </div>
     <div className="max-w-sm mx-auto px-4 py-16 relative rounded text-center w-full">
       <Link to="/">
@@ -50,7 +57,7 @@ const Hero = ({ title }) => (
   </div>
 );
 
-const Contact = ({ phone, phone_formatted, email }) => (
+const HeroContact = ({ phone, phone_formatted, email }) => (
   <div className="contact relative max-w-3xl pin-t pin-l pin-r pt-8 sm:pt-0 px-4 mx-auto w-full">
     <div className="flex flex-wrap items-center justify-around p-4">
       {/* Phone */}
@@ -61,11 +68,11 @@ const Contact = ({ phone, phone_formatted, email }) => (
           className="flex-no-shrink mr-4 w-6"
         >
           <path
-            className="fill-current text-white"
+            className="primary"
             d="M4 2h4a1 1 0 0 1 .98.8l1 5a1 1 0 0 1-.27.9l-2.52 2.52a12.05 12.05 0 0 0 5.59 5.59l2.51-2.52a1 1 0 0 1 .9-.27l5 1c.47.1.81.5.81.98v4a2 2 0 0 1-2 2h-2A16 16 0 0 1 2 6V4c0-1.1.9-2 2-2z"
           />
           <path
-            className="fill-current text-cyan-200"
+            className="secondary"
             d="M14.7 10.7a1 1 0 0 1-1.4-1.4L17.58 5H15a1 1 0 0 1 0-2h5a1 1 0 0 1 1 1v5a1 1 0 0 1-2 0V6.41l-4.3 4.3z"
           />
         </svg>
@@ -82,7 +89,7 @@ const Contact = ({ phone, phone_formatted, email }) => (
           className="flex-no-shrink mr-4 w-6"
         >
           <path
-            className="fill-current text-cyan-200"
+            className="secondary"
             d="M15.6 15.47A4.99 4.99 0 0 1 7 12a5 5 0 0 1 10 0v1.5a1.5 1.5 0 1 0 3 0V12a8 8 0 1 0-4.94 7.4 1 1 0 1 1 .77 1.84A10 10 0 1 1 22 12v1.5a3.5 3.5 0 0 1-6.4 1.97zM12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"
           />
         </svg>
@@ -113,6 +120,61 @@ const Contact = ({ phone, phone_formatted, email }) => (
           </a>
         </p>
       </p>
+    </div>
+  </div>
+);
+
+const Contact = ({ phone, phone_formatted, email }) => (
+  <div className="max-w-3xl mx-auto w-full">
+    <div className="flex flex-wrap w-full">
+      <div className="p-4 w-full md:w-1/2">
+        <h1 className="text-5xl">Contact</h1>
+        {/* Contact Info */}
+        <div className="mb-8">
+          <h2>Contact Information</h2>
+          {/* Phone */}
+          <p className="flex items-center mb-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              className="w-6 mr-4"
+            >
+              <path
+                className="secondary"
+                d="M4 2h4a1 1 0 0 1 .98.8l1 5a1 1 0 0 1-.27.9l-2.52 2.52a12.05 12.05 0 0 0 5.59 5.59l2.51-2.52a1 1 0 0 1 .9-.27l5 1c.47.1.81.5.81.98v4a2 2 0 0 1-2 2h-2A16 16 0 0 1 2 6V4c0-1.1.9-2 2-2z"
+              />
+              <path
+                className="tertiary"
+                d="M14.7 10.7a1 1 0 0 1-1.4-1.4L17.58 5H15a1 1 0 0 1 0-2h5a1 1 0 0 1 1 1v5a1 1 0 0 1-2 0V6.41l-4.3 4.3z"
+              />
+            </svg>
+            <a className="underline" href={`tel:${phone}`}>
+              {phone_formatted}
+            </a>
+          </p>
+          {/* Email */}
+          <p className="flex items-center mb-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              className="w-6 mr-4"
+            >
+              <path
+                className="tertiary"
+                d="M15.6 15.47A4.99 4.99 0 0 1 7 12a5 5 0 0 1 10 0v1.5a1.5 1.5 0 1 0 3 0V12a8 8 0 1 0-4.94 7.4 1 1 0 1 1 .77 1.84A10 10 0 1 1 22 12v1.5a3.5 3.5 0 0 1-6.4 1.97zM12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"
+              />
+            </svg>
+            <a className="underline" href={`mailto:${email}`}>
+              {email}
+            </a>
+          </p>
+        </div>
+        {/* Contact Form */}
+        <ContactForm />
+      </div>
+      <div className="w-full md:w-1/2">
+        <ContactImage />
+      </div>
     </div>
   </div>
 );
