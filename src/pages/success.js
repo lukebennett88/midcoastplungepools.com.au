687fca2b871e868da3cb04d46ffba59e99343aed
Luke Bennett <luke.bennett88@gmail.com>
2019-03-12 20:06:31 +1100

    Move success page

--- src/pages/success.js
+++ src/pages/success.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
-import Layout from '../../components/Layout';
-import Wrapper from '../../components/Wrapper';
-import SEO from '../../components/SEO';
+import Layout from '../components/Layout';
+import Wrapper from '../components/Wrapper';
+import SEO from '../components/SEO';
 
 const SuccessPage = () => (
   <Layout>
