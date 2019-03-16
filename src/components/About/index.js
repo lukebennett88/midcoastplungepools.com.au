84884a7dcf2a0f807ea1b124dff42b6399c0603f
Luke Bennett <luke.bennett88@gmail.com>
2019-03-16 13:05:41 +1100

    Style About component

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,28 +1,29 @@
 import React from 'react';
-import AboutImage from '../Image/About';
+import Image from './Image';
 
 const About = () => (
-  <div
-    className="flex flex-wrap max-w-3xl -mt-10 mb-4 mx-auto w-full"
-    id="about"
-  >
+  <div className="flex flex-wrap max-w-4xl mx-auto w-full" id="about">
     <div className="header-spacer px-4 w-full md:w-1/2">
-      <h2>About</h2>
-      <p>
-        Mid Coast Plunge Pools is a professional and innovative young company
-        dedicated to helping you transform your backyard into a stylish and
-        enjoyable space.
-      </p>
+      <div className="max-w-sm mx-auto">
+        <h2 className="text-3xl md:text-5xl uppercase">About</h2>
+        <p>
+          Mid Coast Plunge Pools is a professional and innovative young company
+          dedicated to helping you transform your backyard into a stylish and
+          enjoyable space.
+        </p>
 
-      <p>
-        We service the entire Mid North Coast (from Newcastle to the Ballina.)
-      </p>
+        <p>
+          We service the entire Mid North Coast (from Newcastle to the Ballina.)
+        </p>
 
-      <p>Call us today to make your dream of owning a pool a reality!</p>
+        <p>Call us today to make your dream of owning a pool a reality!</p>
+      </div>
     </div>
-    <div className="bg-blue-vivid-700 relative w-full md:w-1/2">
-      <div className="image-container opacity-75">
-        <AboutImage />
+    <div className="flex w-full md:w-1/2">
+      <div className="bg-blue-vivid-700 h-full w-full relative">
+        <div className="image-container opacity-75">
+          <Image />
+        </div>
       </div>
     </div>
   </div>
