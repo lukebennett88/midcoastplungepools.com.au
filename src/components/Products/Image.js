ccd7471e3598f88658501cf9ee244b354d1b3135
Luke Bennett <luke.bennett88@gmail.com>
2019-03-16 13:04:54 +1100

    Move image components into folder component folder

---
+++ src/components/Products/Image.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
+
+const Image = () => (
+  <StaticQuery
+    query={graphql`
+      query {
+        placeholderImage: file(relativePath: { eq: "hero.jpg" }) {
+          childImageSharp {
+            fluid(maxWidth: 1178) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+    `}
+    render={data => <Img fluid={data.placeholderImage.childImageSharp.fluid} />}
+  />
+);
+export default Image;
